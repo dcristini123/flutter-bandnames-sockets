a50f9e8b215409838006583ef4beaee8de42ff8d
Diego Cristini <dcristini@fvsa.com>
2021-11-22 17:25:16 -0300

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,143 @@
+import 'dart:io';
+
+import 'package:band_names/models/band.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 3),
+    Band(id: '3', name: 'Héroes', votes: 8),
+    Band(id: '4', name: 'AC/DC', votes: 9),
+    Band(id: '5', name: 'Ramones', votes: 15),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: Text('BandNames', style: TextStyle(color: Colors.black87)),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: bands.length,
+          itemBuilder: (context, i) => _bandTile(bands[i])),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        elevation: 1,
+        onPressed: () {
+          addNewBand();
+        },
+      ),
+    );
+  }
+
+  Widget _bandTile(Band band) {
+    //esto lo hice para evitar que de error x nulo
+    String lv_name = band.name.toString();
+    if (lv_name != null) {
+      lv_name = lv_name.substring(0, 2);
+    }
+
+    ///
+    ///
+    ///
+    return Dismissible(
+      key: Key(band.id.toString()),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        print('direction: $direction');
+        // todo; llamar al borrado en server
+      },
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red,
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Delete Band',
+              style: TextStyle(color: Colors.white),
+            )),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(lv_name),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name.toString()),
+        trailing: Text('${band.votes}', style: TextStyle(fontSize: 20)),
+        onTap: () {
+          print(band.name);
+        },
+      ),
+    );
+  }
+
+  addNewBand() {
+    final textController = new TextEditingController();
+
+    if (Platform.isAndroid) {
+      //android
+
+      return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              title: Text('New band name:'),
+              content: TextField(
+                controller: textController,
+              ),
+              actions: <Widget>[
+                MaterialButton(
+                    child: Text('Add'),
+                    elevation: 5,
+                    textColor: Colors.blue,
+                    onPressed: () => addBandToList(textController.text))
+              ]);
+        },
+      );
+      print("????");
+    }
+
+    showCupertinoDialog(
+        context: context,
+        builder: (_) {
+          return CupertinoAlertDialog(
+            title: Text('New band name'),
+            content: CupertinoTextField(
+              controller: textController,
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text('Add'),
+                onPressed: () => addBandToList(textController.text),
+              ),
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text('Dismiss'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        });
+  }
+
+  void addBandToList(String name) {
+    print(name);
+    if (name.length > 1) {
+      this
+          .bands
+          .add(new Band(id: DateTime.now().toString(), name: name, votes: 0));
+      setState(() {});
+    }
+    Navigator.pop(context);
+  }
+}
